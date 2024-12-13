38f05442fc111966cc9f68f52eb8ddc3bc5672e1
nqd182 <nguyendung180203@gmail.com>
2024-12-13 23:45:18 +0700

    atsh

---
+++ test.cs
@@ -0,0 +1,12 @@
+namespace test{
+    partial class Test{
+        public int ID{get; set};
+        public string Name{get; set};
+        public double Value{get; set};
+
+        static void main(){
+            Test 1 = new Test();
+
+        }
+    }
+}
